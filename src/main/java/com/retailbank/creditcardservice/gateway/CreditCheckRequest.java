1ecab5ac9433312612c8dd3233666620e75967d5
Morgan Berg <morgan.berg@knowit.se>
2019-09-28 23:18:03 +0200

    Implementing regex contracts.

--- src/main/java/com/retailbank/creditcardservice/gateway/CreditCheckRequest.java
+++ src/main/java/com/retailbank/creditcardservice/gateway/CreditCheckRequest.java
@@ -3,9 +3,14 @@ package com.retailbank.creditcardservice.gateway;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+
 @RequiredArgsConstructor
 public class CreditCheckRequest {
 
     @Getter
     private final int citizenNumber;
+
+    @Getter
+    private final String requestedDate = LocalDate.now().toString();
 }
